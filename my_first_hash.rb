2f2dfc764b22094441e64b44951b120d71a41416
Nicky Liu <nickyliu91@gmail.com>
2018-08-14 10:46:58 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,6 +27,8 @@ def retrieval
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
+  shipping_manifest["oil paintings"]
+
 end
 
 def adding
@@ -37,15 +39,15 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+  shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
 
-  shipping_manifest["oil paintings"]
+  shipping_manifest
 
 end
